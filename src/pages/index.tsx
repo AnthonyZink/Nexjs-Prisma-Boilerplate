9c5de4b5fa0b9434efd2ccb0e57d5c1e12fe82fe
Anthony <anthony.zink@outlook.fr>
2021-05-08 17:24:29 +0200

    add tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { PrismaClient } from "@prisma/client";
-import type {GetStaticProps} from "next";
 
-export const getStaticProps: GetStaticProps = async () => {
-    const prisma = new PrismaClient();
-
-    const users = await prisma.users.findMany();
-    return {
-        props: {
-            users
-        }, // will be passed to the page component as props
-      }
-}
-
-export default function Home({users}) {
+export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
@@ -24,12 +11,7 @@ export default function Home({users}) {
 
             <h1>Prisma</h1>
 
-            <main className={styles.main}>
-                <ul>
-                    {users.map(({id, firstname}) => <li>{id} - {firstname}</li>)}
-                </ul>
-                
-            </main>
+            <main className={styles.main}></main>
         </div>
     );
 }
